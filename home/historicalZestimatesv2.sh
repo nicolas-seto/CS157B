83390ada5551f15e75d2f72b65d5e44a690d2fc8
nicolas_seto <nicolasseto@gmail.com>
2014-04-19 21:35:42 -0700

    Series is the same as zpid

--- home/historicalZestimatesv2.sh
+++ home/historicalZestimatesv2.sh
@@ -6,26 +6,25 @@ NEW_FILE="historicalZestimates.csv"
 
 # Extract all the zpids in the first column
 zpids=$(cut -d , -f 1 ${PARSE_FILE})
-echo "date,value,zpid" > "$NEW_FILE"
+echo "date,value,series,zpid" > "$NEW_FILE"
 
 for line in $(echo ${zpids}) ; do
     if [ $line == "zpid" ] ; then
         continue
     else
         echo "Extracting past zestimates for home $line . . ."
-        curl -o "${line}.txt" "${URL}${line}"
+        homeValues=$(curl "${URL}${line}")
+        homeValuesComma=$(echo "$homeValues" | sed "s/\t/,/g")
         (( count = 0 ))
         echo "Parsing home data for relevant data . . ."
-        while read line2 ; do
+        for line2 in $(echo "$homeValuesComma") ; do
             if [ count == 0 ] ; then
                 continue
             elif [[ $line2 =~ "This home" ]] ; then
-                echo "$line2" | sed "s/\t/,/g" | sed "s/\([0-9]\+\)\/\([0-9]\+\)\/\([0-9]\+\)/\3-\1-\2/g" | sed "s/,This home//g" >> "$NEW_FILE"
+                echo "$line2" | sed "s/\([0-9]\+\)\/\([0-9]\+\)\/\([0-9]\+\)/\3-\1-\2/g" | sed "s/,This home//g" >> "$NEW_FILE"
             fi
             (( count++ ))
-        done < "${line}.txt"
-        
-        rm -f "${line}.txt"
+        done
     fi
 done
 echo "DONE\n"
