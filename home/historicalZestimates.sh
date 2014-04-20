8d3ce3d5ae288b364e94e193109c8b6b743e17c7
nicolas_seto <nicolasseto@gmail.com>
2014-04-19 18:08:39 -0700

    Fixed logic: Removed IFS

--- home/historicalZestimates.sh
+++ home/historicalZestimates.sh
@@ -6,28 +6,25 @@ NEW_FILE="historicalZestimates.csv"
 
 # Extract all the zpids in the first column
 zpids=$(cut -d , -f 1 ${PARSE_FILE})
+echo "date,value,series,zpid" > "$NEW_FILE"
 
-old_IFS=$IFS
-IFS=$'\n'
 for line in $(echo ${zpids}) ; do
-    if [ $line -eq "zpid" ] ; then
+    if [ $line == "zpid" ] ; then
         continue
     else
-        echo "Extracting past zestimates for home $line . . .\n"
+        echo "Extracting past zestimates for home $line . . ."
         homeOutput=$(curl "${URL}${line}")
         homeOutputComma=$(echo "${homeOutput}" | sed "s/\t/,/g")
         (( count = 0 ))
-        echo "date,value,series,zpid\n" > "$NEW_FILE"
-        echo "Parsing home data for relevant data . . .\n"
+        echo "Parsing home data for relevant data . . ."
         for line2 in $(echo ${homeOutputComma}) ; do
             if [ count == 0 ] ; then
                 continue
             elif [[ $line2 =~ "This home" ]] ; then
-                echo "$line2" | sed "s/(\d+)\/(\d+)\/(\d+)/\3-\1-\2/g" | sed "s/This home/$line\n/g" > "$NEW_FILE"
+                echo "$line2" | sed "s/(\d+)\/(\d+)\/(\d+)/\3-\1-\2/g" | sed "s/This home/${line}\\n/g" >> "$NEW_FILE"
             fi
             (( count++ ))
         done
     fi
 done
-IFS=$old_IFS
 echo "DONE\n"
